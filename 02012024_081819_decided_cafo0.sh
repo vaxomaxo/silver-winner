21c8bc161beabac5c9ac06ba7227a806902aa840
jamkakalia <jorbenadzenatia028@gmail.com>
2024-01-02 08:18:19 +0000

    Updating decided

---
+++ 02012024_081819_decided_cafo0.sh
@@ -0,0 +1,1 @@
+The abbreviation Xmas for the word Christmas is of Greek origin. Since the word for Christ in the Greek language is Xristos, which starts with the letter "X," they started putting the X in place of Christ and came up with the short form for the word Christmas
